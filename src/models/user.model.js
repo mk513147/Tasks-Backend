ddf7711b7defd822a9e5bb4b67175cc9b6792122
Mohit kumar <mk513147@gmail.com>
2026-01-27 20:52:19 +0530

    feat: added user model

---
+++ src/models/user.model.js
@@ -0,0 +1,45 @@
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt'
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timestamps: true
+})
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+userSchema.methods.comparePassword = async function (givenPassword) {
+    return await bcrypt.compare(givenPassword, this.password);
+}
+
+const User = model('User', userSchema);
+export default User;
