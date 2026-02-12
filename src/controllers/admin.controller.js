f800b98fdbdfb5b5bac340a1f2285c4e49831143
Mohit kumar <mk513147@gmail.com>
2026-02-12 20:54:24 +0530

    feat: added updateUserRole function

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,10 +1,95 @@
 import { apiError } from "../utils/apiError.utils";
 import { apiResponse } from "../utils/apiResponse.utils";
 import User from "../models/user.model";
+import { queryBuilder } from "../utils/queryBuilder.utils.js";
+import mongoose from "mongoose";
 
 // get all users  (with filter for active, deleted, all)
 // get users by id
 // update user role
 // delete user (soft delete)
 // restore user
-// 
+
+export const getUsers = async (req, res, next) => {
+    try {
+        const { includeDeleted, includeInactive } = req.query;
+        const baseFilters = {};
+
+        if (includeDeleted !== "true") {
+            baseFilters.deletedAt = null;
+        }
+        if (includeInactive !== "true") {
+            baseFilters.isActive = true;
+        }
+
+        const { filters, sort, limit, page, skip } = queryBuilder({
+            baseFilters,
+            query: req.query,
+            searchableFields: ["fullName", "email", "username"],
+        })
+
+        const [users, total] = await Promise.all([
+            User.find(filters).select("-password").sort(sort).skip(skip).limit(limit).lean(),
+            User.countDocuments(filters)
+        ])
+
+        return apiResponse(res, 200, "Users retrieved successfully", {
+            users,
+            pagination: {
+                total,
+                page,
+                limit,
+                totalPages: Math.ceil(total / limit),
+                hasNextPage: page * limit < total,
+                hasPrevPage: page > 1
+            }
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getUserById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return next(apiError(400, "Invalid User ID"));
+        }
+
+        const user = await User.findById(id).select("-password").lean();
+        if (!user) {
+            return next(apiError(404, "User not found"));
+        }
+
+        return apiResponse(res, 200, "User retrieved successfully", { user })
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const updateUserRole = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { role } = req.body;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return next(apiError(400, "Invalid User ID"));
+        }
+
+        const allowedRoles = ["user", "admin"];
+
+        if (!allowedRoles.includes(role)) {
+            return next(apiError(400, "Invalid role. Allowed values are 'user' and 'admin'"));
+        }
+
+        const updatedUser = await User.findOneAndUpdate({ _id: id, deletedAt: null }, { $set: { role } }, { new: true, runValidators: true, select: "-password" });
+        if (!updatedUser) {
+            return next(apiError(404, "User not found or has been deleted"));
+        }
+
+        return apiResponse(res, 200, "User role updated successfully")
+    } catch (error) {
+        next(error);
+    }
+}
