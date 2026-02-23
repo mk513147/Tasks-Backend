24b662743f94401754548034da7438e8d443b8d3
Mohit kumar <mk513147@gmail.com>
2026-02-23 21:43:36 +0530

    fix: fixed a minor bug feat: added cookie parser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import taskRouter from './routes/task.route.js';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
@@ -9,10 +10,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/tasks', taskRouter);
 app.use('/api/users', userRouter);
-app.use('api/auth', authRouter);
+app.use('/api/auth', authRouter);
 
 app.get('/', (req, res) => {
     res.send('Baba Ji ki booty!!');
